a2890eada66a66044da11b5dcd9df981f24fb974
yexxx <yx19321@gmail.com>
2026-02-28 22:32:15 +0800

    Flatten tool registry module structure

--- tests/test_tool_registry.py
+++ tests/test_tool_registry.py
@@ -1,6 +1,6 @@
 import pytest
 
-from agent_teams.tools.registry.defaults import build_default_registry
+from agent_teams.tools.defaults import build_default_registry
 
 
 def test_registry_rejects_unknown_tools() -> None:

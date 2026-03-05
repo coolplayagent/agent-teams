d0da9941d775d56c5f5131ff123d698ef25e7495
Tangdeping <t00509066@china.huawei.com>
2026-03-05 12:00:40 +0800

    feat: add setup.bat and quality tool

--- setup.bat
+++ setup.bat
@@ -11,15 +11,33 @@ if %errorlevel% neq 0 (
 echo Checking uv...
 uv --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo [Error] uv not found. Please install uv first: https://github.com/astral-sh/uv
-    exit /b 1
+    echo uv not found, installing uv......
+    setlocal enabledelayedexpansion
+    pip install uv
+    if %errorlevel% neq 0 (
+        echo [ERROR] uv install failed
+        pause
+        exit /b 1
+    )
 )
 
+if exist uv.lock del /f /q uv.lock >nul 2>&1
+
 echo Installing dependencies (including dev tools)...
-uv sync --extra dev
+set UV_NATIVE_TLS=1
+uv sync --all-extras --index-strategy unsafe-best-match
 if %errorlevel% neq 0 (
     echo [Error] Dependency installation failed.
     exit /b 1
+)
+
+echo install git hooks....
+uv run pre-commit install
+if %errorlevel% neq 0 (
+    echo.
+    echo [WARNING] Git Hooks install failed
+) else (
+    echo Git Hooks install successful
 )
 
 echo Environment setup completed.
